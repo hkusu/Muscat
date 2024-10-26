03624aa24b864f83eaef3ca98bf4066dc3d509b8
Hiroyuki Kusu <kusu0806@gmail.com>
2024-10-26 10:13:40 +0900

    Change interface

--- muscat-compose/src/commonMain/kotlin/io/github/hkusu/muscat/compose/ComposeExt.kt
+++ muscat-compose/src/commonMain/kotlin/io/github/hkusu/muscat/compose/ComposeExt.kt
@@ -1,6 +1,5 @@
 package io.github.hkusu.muscat.compose
 
-import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -9,37 +8,32 @@ import io.github.hkusu.muscat.core.Action
 import io.github.hkusu.muscat.core.Event
 import io.github.hkusu.muscat.core.State
 import io.github.hkusu.muscat.core.Store
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flowOf
 
 @Suppress("unused")
 @Composable
-inline fun <reified S : State> State.render(block: @Composable S.() -> Unit) {
-    if (this is S) {
-        block(this)
+inline fun <reified S : State> Contract<*, *, *>.render(block: S.() -> Unit) {
+    if (state is S) {
+        block(state)
     }
 }
 
-@SuppressLint("ComposableNaming")
+@Suppress("unused")
 @Composable
-fun <E : Event> E?.handle(block: CoroutineScope.(E) -> Unit) {
-    this?.let {
-        LaunchedEffect(it) {
-            block(it)
+inline fun <reified E : Event> Contract<*, *, *>.handle(crossinline block: E.() -> Unit) {
+    LaunchedEffect(Unit) {
+        event.filter { it is E }.collect {
+            block(it as E)
         }
     }
-}
-
-@Suppress("unused")
-@SuppressLint("ComposableNaming")
-@Composable
-fun <E : Event> Contract<*, *, E>.handleEvents(block: CoroutineScope.(E) -> Unit) {
-    event?.handle(block = block)
 }
 
 data class Contract<S : State, A : Action, E : Event>(
     val state: S,
     val dispatch: (A) -> Unit = {},
-    internal val event: E? = null,
+    val event: Flow<E> = flowOf(),
 )
 
 @Suppress("unused")
@@ -48,6 +42,5 @@ fun <S : State, A : Action, E : Event> contract(
     store: Store<S, A, E>,
 ): Contract<S, A, E> {
     val state by store.state.collectAsState()
-    val event by store.event.collectAsState(initial = null)
-    return Contract(state = state, dispatch = store::dispatch, event = event)
+    return Contract(state = state, dispatch = store::dispatch, event = store.event)
 }
